4b0a43fa12bd039a269f8571bb50b9cdf918af0f
usuario <usuario@192.168.56.1>
2018-11-12 13:23:13 +0100

    subida de la clase logica de carrera

--- GestionCorredoresycarreras/src/logica/LogiCarrera.java
+++ GestionCorredoresycarreras/src/logica/LogiCarrera.java
@@ -2,10 +2,13 @@
 package logica;
 
 import DTO.Carrera;
+import DTO.Corredor;
+import DTO.TiemposCorredor;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 import utils.MisExcepciones;
 
@@ -71,8 +74,75 @@ public class LogiCarrera implements Serializable {
   return true;
   }
   //colecion corredores en la carrera
-  
-  
-  
-  
+  private void checkAniadirCorredor(Carrera carrera,TiemposCorredor corredor) throws MisExcepciones.CarrCerrada,MisExcepciones.TooCorredores,MisExcepciones.CorredorIgual{
+  if(corredor == null){
+  throw new IllegalArgumentException("corredor imposible de ser null");
+  }else if(carrera.getListacorredores().contains(corredor)){
+  throw new MisExcepciones.CorredorIgual();
+  }else if(carrera.isCarrCerrada()){
+  throw new MisExcepciones.CarrCerrada();
+  }else if(carrera.getListacorredores().size()>=carrera.getMaxCorredor()){
+  throw new MisExcepciones.TooCorredores();
+  }
+  }
+  public boolean anadirCorredor(Carrera carrera,Corredor corredor) throws MisExcepciones.CarrCerrada,MisExcepciones.TooCorredores,MisExcepciones.CorredorIgual{
+  List<TiemposCorredor>corredoresDorsal = new ArrayList<>(carrera.getListacorredores());
+  Collections.sort(corredoresDorsal);
+  String dorsal=" ";
+  if(corredoresDorsal.isEmpty()){
+  dorsal="1";
+  }else{
+  TiemposCorredor ultimoCorredor=corredoresDorsal.get(corredoresDorsal.size()-1);
+  dorsal=Integer.toString(Integer.parseInt(ultimoCorredor.getDorsal())+1);
+  }
+  TiemposCorredor tiemposCorredor= new TiemposCorredor(carrera.getId(),corredor,dorsal);
+  checkAniadirCorredor(carrera,tiemposCorredor);
+  return carrera.aceptarCorredor(tiemposCorredor);
+  }
+  public boolean anadirCorredores(Carrera carrera,List<Corredor>corredores) throws MisExcepciones.CarrCerrada,MisExcepciones.TooCorredores,MisExcepciones.CorredorIgual{
+  boolean ok= true;
+  for (Corredor corredor:corredores){
+  if(!anadirCorredor(carrera,corredor)){
+  ok=false;
+  }
+  }
+  return ok;
+  }
+  private void checkCorredor(Carrera carrera,TiemposCorredor corredor) throws MisExcepciones.NoCorredor,MisExcepciones.CarrCerrada{
+  if(!carrera.getListacorredores().contains(corredor)){
+  throw new MisExcepciones.NoCorredor();
+  }else if(carrera.isCarrCerrada()){
+  throw new MisExcepciones.CarrCerrada();
+  }
+  }
+  public boolean deCorredor(Carrera carrera,TiemposCorredor corredor) throws MisExcepciones.NoCorredor,MisExcepciones.CarrCerrada{
+  checkCorredor(carrera,corredor);
+  return carrera.borrarCorredor(corredor);
+  }
+
+public boolean deCorredores(Carrera carrera,List<TiemposCorredor>corredores) throws MisExcepciones.NoCorredor,MisExcepciones.CarrCerrada{
+Iterator<TiemposCorredor> it=corredores.iterator();
+while(it.hasNext()){
+TiemposCorredor corredor=it.next();
+checkCorredor(carrera,corredor);
+carrera.borrarCorredor(corredor);
+
+}
+return true;
+}
+//baja corredor
+public static boolean bajaCorredor(Corredor corredor){
+boolean ok=true;
+Iterator<Carrera> iteratorCarrera=LogiCarrera.getInstance().getCarreras().iterator();
+while(iteratorCarrera.hasNext()){
+Carrera carrera=iteratorCarrera.next();
+if(!carrera.isCarrCerrada()&& carrera.contenedorCorredor(corredor.getDni())){
+if(!carrera.borrarCorredor(corredor.getDni())){
+ok=false;
+}
+}
+}
+return ok;
+}
+
 }
