9a2b5b25812a3be897380d46672425a7cd3449c7
usuario <usuario@192.168.56.1>
2018-11-13 12:06:50 +0100

    ficherocsv mas FicheroObjetos

---
+++ GestionCorredoresycarreras/src/utils/FicheroObjetos.java
@@ -0,0 +1,132 @@
+
+package utils;
+
+//gestiona la transferencia de objetos .abrir antes cerrarlo despues
+
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.util.Objects;
+
+
+
+
+
+public class FicheroObjetos<T extends Objects> {
+    //atributos
+  private File fichero;
+  private ObjectInputStream sio;
+  private FileOutputStream sof;
+  private ObjectOutputStream soo;
+  private ADD ad;
+  //constructor
+
+    public FicheroObjetos(File fichero) {
+        this.fichero=fichero;
+    }
+  //metodos 
+    //abrir fichero
+    public void abrirFicheroLector() {
+    try{
+    sio= new ObjectInputStream(new FileInputStream(fichero));
+    }catch(FileNotFoundException e){
+        System.out.println("el fichero no existe");
+    }catch(IOException e){
+        System.out.println("Error al abrir el fichero:"+e.getMessage());
+    }
+    }
+    
+    public void abrirFicheroEscritor(boolean add) throws FileNotFoundException, IOException{
+    try{
+    sof=new FileOutputStream(fichero,add);
+    soo=new ObjectOutputStream(sof);
+    ad=new ADD(sof);
+       }catch(FileNotFoundException e){
+        System.out.println("El fichero no existe");
+       }catch(IOException e){
+        System.out.println("Error al abrir fichero:"+fichero.getName());
+       }
+    }
+    //cerrar fichero
+    public void cerrarFicheroLector(){
+    try{
+    sio.close();
+    }catch(IOException e){
+        System.out.println("Error al cerrar fichero");
+    }
+    }
+    public void cerrarFicheroEscritor(){
+    try{
+    sof.close();
+    soo.close();
+    ad.close();
+        }catch(IOException e){
+        System.out.println("Error al cerrar fichero");
+        }
+    }
+    //leer un objeto del fichero
+    public T leerObjeto() {
+    try{
+    return(T) sio.readObject();
+    }catch(ClassNotFoundException e){
+        System.out.println("no se ha encontrado");
+        return null;
+    }catch (EOFException e){
+    return null;
+    }catch (IOException e){
+    return null;
+    }
+    }
+    //grabar
+    public void grabarPrimerObjeto(T x){
+    try{
+    soo.writeObject(x);
+    }catch (IOException e){
+        System.out.println("Error al grabar el objeto: "+e.getMessage());
+    }
+     }
+    public void grabarObjeto(T x){
+    try{
+    ad.writeObject(x);
+    } catch (IOException e){
+        System.out.println("Error al grabar : "+e.getMessage());
+    }
+    }
+    //transferir
+    public void TransferirObjeto(File entrada,File salida) throws IOException{
+    FicheroObjetos<T> lector= new FicheroObjetos<>(entrada);
+    lector.abrirFicheroLector();
+    FicheroObjetos<T> escritor= new FicheroObjetos<>(salida);
+    escritor.abrirFicheroEscritor(false);
+    T ob=lector.leerObjeto();
+    if(ob !=null){
+    escritor.grabarPrimerObjeto(ob);
+    while((ob=lector.leerObjeto()) !=null){
+    escritor.grabarObjeto(ob);
+    }
+    }
+    lector.cerrarFicheroLector();
+    escritor.cerrarFicheroEscritor();
+    }
+    public class ADD extends ObjectOutputStream{
+ //constructor
+        public ADD(OutputStream os)throws IOException {
+        super(os);
+        }
+        public ADD()throws IOException{
+        super();
+        }
+        
+        @Override
+        protected void writeStreamHeader()throws IOException{
+        }
+        
+    
+    }
+}
